db0286376206dfbf538a2093d148c83679318451
Tu Hoang Gia Phuc <tuhoanggiap@gmail.com>
2025-11-26 20:32:22 +0700

    Created the header of the ProQuiz page
    Make the header of the website including the logo so the users can click back to the homepage, a search bar for searching function, a Create a quiz button a an avatar to the user account

---
+++ frontend/src/Components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import './Header.scss';
+import { Link } from 'react-router-dom';
+import LogoImage from '../../assets/logo.png';
+import AvatarImage from '../../assets/avatar.png';
+import Button from '@mui/material/Button';
+export default function Header() {
+  return (
+    <header className="header">
+      <Link to ="/" className="header__logo">
+        <img src ={LogoImage} alt="App Logo" className="header__logo-image" />
+      </Link>
+        <input type ="search" placeholder="Search." className="header__search-bar" />
+        <Button variant="contained"  className="header__quiz-button">
+          Create a quiz
+        </Button>
+        <Link to ="" className="header__avatar">
+            <img src ={AvatarImage} alt="User Avatar" className="header__avatar-image" />
+        </Link>
+    </header>
+  );
+}
--- frontend/src/Homepage/Homepage.tsx
+++ frontend/src/Homepage/Homepage.tsx
@@ -1,11 +1,14 @@
-import "./Homepage.css";
-
+import './Homepage.scss';
+import Header from '../Components/Header/Header';
 function Homepage() {
   return (
+      <>
+    <Header />
     <div className="homepage-container">
       <h1>Welcome to ProQuiz</h1>
       <p>Your ultimate platform for creating and taking quizzes!</p>
     </div>
+      </>
   );
 }
 export default Homepage;
