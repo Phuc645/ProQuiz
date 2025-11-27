429788ac5135ad58109df6557f1cdcf4b6c26077
Tu Hoang Gia Phuc <tuhoanggiap@gmail.com>
2025-11-27 20:14:06 +0700

    Make the homepage, including the featured section and the Retake the quiz section

--- frontend/src/Homepage/Homepage.tsx
+++ frontend/src/Homepage/Homepage.tsx
@@ -1,10 +1,89 @@
 import './Homepage.scss';
 import Header from '../Components/Header/Header';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import UserImage from '../assets/avatar.png';
+
 function Homepage() {
   return (
       <>
-    <Header />
-
+        <Header />
+        <h2 className="homepage">Featured</h2>
+              <div className = "featured_container">
+                <Card className="featured_card">
+                  <CardContent>
+                    <h3>History of Romumania</h3>
+                    <div className="user"><img className="user-avatar" src ={UserImage} alt="User"/><p>User1</p></div>
+                      <p>Category: Social Science</p>
+                  </CardContent>
+                </Card>
+                  <Card className="featured_card">
+                      <CardContent>
+                          <h3>History of Romumania</h3>
+                          <div className="user"><img className="user-avatar" src ={UserImage} alt="User"/><p>User1</p></div>
+                          <p>Category: Social Science</p>
+                      </CardContent>
+                  </Card>
+                  <Card className="featured_card">
+                      <CardContent>
+                          <h3>History of Romumania</h3>
+                          <div className="user"><img className="user-avatar" src ={UserImage} alt="User"/><p>User1</p></div>
+                          <p>Category: Social Science</p>
+                      </CardContent>
+                  </Card>
+                  <Card className="featured_card">
+                      <CardContent>
+                          <h3>History of Romumania</h3>
+                          <div className="user"><img className="user-avatar" src ={UserImage} alt="User"/><p>User1</p></div>
+                          <p>Category: Social Science</p>
+                      </CardContent>
+                  </Card>
+                  <Card className="featured_card">
+                      <CardContent>
+                          <h3>History of Romumania</h3>
+                          <div className="user"><img className="user-avatar" src ={UserImage} alt="User"/><p>User1</p></div>
+                          <p>Category: Social Science</p>
+                      </CardContent>
+                  </Card>
+              </div>
+          <h2 className="homepage">Retake the quiz</h2>
+              <div className = "featured_container">
+                  <Card className="featured_card">
+                      <CardContent>
+                          <h3>History of Romumania</h3>
+                          <div className="user"><img className="user-avatar" src ={UserImage} alt="User"/><p>User1</p></div>
+                          <p>Category: Social Science</p>
+                      </CardContent>
+                  </Card>
+                  <Card className="featured_card">
+                      <CardContent>
+                          <h3>History of Romumania</h3>
+                          <div className="user"><img className="user-avatar" src ={UserImage} alt="User"/><p>User1</p></div>
+                          <p>Category: Social Science</p>
+                      </CardContent>
+                  </Card>
+                  <Card className="featured_card">
+                      <CardContent>
+                          <h3>History of Romumania</h3>
+                          <div className="user"><img className="user-avatar" src ={UserImage} alt="User"/><p>User1</p></div>
+                          <p>Category: Social Science</p>
+                      </CardContent>
+                  </Card>
+                  <Card className="featured_card">
+                      <CardContent>
+                          <h3>History of Romumania</h3>
+                          <div className="user"><img className="user-avatar" src ={UserImage} alt="User"/><p>User1</p></div>
+                          <p>Category: Social Science</p>
+                      </CardContent>
+                  </Card>
+                  <Card className="featured_card">
+                      <CardContent>
+                          <h3>History of Romumania</h3>
+                          <div className="user"><img className="user-avatar" src ={UserImage} alt="User"/><p>User1</p></div>
+                          <p>Category: Social Science</p>
+                      </CardContent>
+                  </Card>
+                </div>
       </>
   );
 }
