0a8ed09bb9c8229ea7e9c877cb626d244b59f004
Tu Hoang Gia Phuc <tuhoanggiap@gmail.com>
2025-12-09 20:12:05 +0700

    Update Finished component to display results with ButtonComponent; add conditional rendering for result display

--- frontend/src/Finished/Finished.tsx
+++ frontend/src/Finished/Finished.tsx
@@ -1,13 +1,28 @@
 import './Finished.scss';
 import CircularProgress from '@mui/material/CircularProgress';
+import ButtonComponent from "../Components/ButtonComponent/ButtonComponent.tsx";
 export default function Finished() {
-    return (
-        <div className="finished-container">
-            <div className="finished-text">
-                <p className="finished-title">Finished</p>
-                <p className="finished-subtitle">Calculating your result</p>
+    const test: boolean = false;
+    if (test) {
+        return (
+            <div className="finished-container">
+                <div className="finished-text">
+                    <p className="finished-title">Finished</p>
+                    <p className="finished-subtitle">Calculating your result</p>
+                </div>
+                <CircularProgress />
             </div>
-            <CircularProgress />
-        </div>
-    );
+        );
+    }
+    else {
+        return (
+            <div className="finished-container">
+                <p className="finished-title">Here's your result</p>
+                <ButtonComponent style={{ marginTop: '20px', backgroundColor: '#7fb8c1', width: '150px', height: '40px' }}>
+                    Show result
+                </ButtonComponent>
+            </div>
+        )
+    }
+
 }
