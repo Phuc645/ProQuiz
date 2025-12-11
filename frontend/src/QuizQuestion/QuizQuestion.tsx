ce253069a7e588d1065f601dc23f379b8b3713b3
Tu Hoang Gia Phuc <tuhoanggiap@gmail.com>
2025-12-11 20:43:57 +0700

    Update QuizQuestion styles and structure; enhance layout and improve accessibility

--- frontend/src/QuizQuestion/QuizQuestion.tsx
+++ frontend/src/QuizQuestion/QuizQuestion.tsx
@@ -5,16 +5,14 @@ export default function QuizQuestion() {
         <div className="quiz-question-container">
             <progress className="quiz-progress-bar" value={70} max={100}></progress>
             <div className="quiz-question-content">
-                <p className="quiz-question-number">Question </p>
+                <h1 className="quiz-question-number">Question </h1>
                 <p className ="quiz-question-text">What is the capital of France?</p>
             </div>
             <div className="quiz-answers">
-                <div className="quiz-answers">
-                    <Button variant="contained" className="quiz-answer-button" sx={{ textTransform: 'none', color: '#fff' }}>A) Berlin</Button>
-                    <Button variant="contained" className="quiz-answer-button" sx={{ textTransform: 'none', color: '#fff' }}>B) Madrid</Button>
-                    <Button variant="contained" className="quiz-answer-button" sx={{ textTransform: 'none', color: '#fff' }}> Paris</Button>
-                    <Button variant="contained" className="quiz-answer-button" sx={{ textTransform: 'none', color: '#fff' }}>D) Rome</Button>
-            </div>
+                <Button variant="contained" className="quiz-answer-button" sx={{ textTransform: 'none', color: '#fff' }}>A) Berlin</Button>
+                <Button variant="contained" className="quiz-answer-button" sx={{ textTransform: 'none', color: '#fff' }}>B) Madrid</Button>
+                <Button variant="contained" className="quiz-answer-button" sx={{ textTransform: 'none', color: '#fff' }}> Paris</Button>
+                <Button variant="contained" className="quiz-answer-button" sx={{ textTransform: 'none', color: '#fff' }}>D) Rome</Button>
             </div>
         </div>
 
