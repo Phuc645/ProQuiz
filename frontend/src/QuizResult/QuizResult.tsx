2c5f0c21fcf82d9f4213bbbddd5f0f24fe46b1ac
Tu Hoang Gia Phuc <tuhoanggiap@gmail.com>
2025-12-26 15:48:49 +0700

    Finish the quiz result page
    Enhance QuizResult component with detailed incorrect answers display and action buttons

--- frontend/src/QuizResult/QuizResult.tsx
+++ frontend/src/QuizResult/QuizResult.tsx
@@ -1,6 +1,14 @@
 import './QuizResult.scss'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import ButtonComponent from '../Components/ButtonComponent/ButtonComponent';
 
 export default function QuizResult() {
+    const items = [
+        {id: 1, question: "What is 2 + 2?", yourAnswer: "3", correctAnswer: "4"},
+        {id: 2, question: "What is the capital of France?", yourAnswer: "Berlin", correctAnswer: "Paris"},
+        {id: 3, question: "What is the largest planet in our solar system?", yourAnswer: "Earth", correctAnswer: "Jupiter"},
+    ]
     return (
         <div className="quiz-result-container">
             <div className="quiz-result-header">
@@ -9,8 +17,26 @@ export default function QuizResult() {
                 <p>Incorrect Answers: 6/20</p>
             </div>
             <div className="quiz-result-body">
+                <h3>Incorrect Answers explanation</h3>
+                <div className="quiz-result-list">
+                    {items.map(item => (
+                        <Card key={item.id}>
+                            <CardContent>
+                            <strong>Question:</strong> {item.question} <br />
+                            <strong>Your Answer:</strong> {item.yourAnswer} <br />
+                            <strong>Correct Answer:</strong> {item.correctAnswer}
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
             </div>
             <div className="quiz-result-actions">
+                <ButtonComponent style={{ backgroundColor: '#7fb8c1' }}>
+                        Retake the quiz
+                </ButtonComponent>
+                <ButtonComponent style={{ backgroundColor: 'transparent' }}>
+                        Go back to the homepage
+                </ButtonComponent>
             </div>
         </div>
     );
